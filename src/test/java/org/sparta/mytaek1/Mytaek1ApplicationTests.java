c37cbbc8cebae720258a0bfb8590efd65ed9f4e3
KimDabomi <cdabomi@nate.com>
2024-01-27 18:29:12 +0900

    init: 초기 프로젝트 생성

---
+++ src/test/java/org/sparta/mytaek1/Mytaek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.mytaek1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mytaek1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
