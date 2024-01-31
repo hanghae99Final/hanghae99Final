7dbee58529c18084b92d84106804bff136f58c4f
crescent0kt <gyungtaek0@gmail.com>
2024-01-31 20:39:34 +0900

    feat: Product 생성 기능 추가

--- src/main/java/org/sparta/mytaek1/domain/product/controller/ProductController.java
+++ src/main/java/org/sparta/mytaek1/domain/product/controller/ProductController.java
@@ -1,22 +1,28 @@
 package org.sparta.mytaek1.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sparta.mytaek1.domain.product.dto.CreateProductRequestDto;
+import org.sparta.mytaek1.domain.product.dto.CreateProductResponseDto;
+import org.sparta.mytaek1.domain.product.dto.UpdateProductStockRequestDto;
+import org.sparta.mytaek1.domain.product.dto.UpdateProductStockResponseDto;
 import org.sparta.mytaek1.domain.product.service.ProductService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/api")
+@RestController
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
-    @GetMapping("/products/{productId}")
-    public String findProduct(@PathVariable Long productId, Model model){
-        model.addAttribute("productFind",productService.findProduct(productId));
-        return "Product";
+    @PostMapping("/api/products")
+    public CreateProductResponseDto createProduct(@RequestBody CreateProductRequestDto requestDto){
+        return productService.createProduct(requestDto);
+    }
+
+    @PutMapping("/api/products/{productId}/stock")
+    public UpdateProductStockResponseDto updateProductStock(@RequestBody UpdateProductStockRequestDto requestDto){
+        return productService.updateProductStock(requestDto);
     }
 }
