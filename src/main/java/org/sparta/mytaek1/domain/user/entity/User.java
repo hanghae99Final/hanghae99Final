4e840ed420ee0a849a9cfaeddc0e0ffc6d0ed8a7
KimDabomi <cdabomi@nate.com>
2024-01-31 16:53:33 +0900

    feat: user entity,controller

---
+++ src/main/java/org/sparta/mytaek1/domain/user/entity/User.java
@@ -0,0 +1,31 @@
+package org.sparta.mytaek1.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String userEmail;
+
+    public User(String userName, String userEmail, String password) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.password = password;
+    }
+}
