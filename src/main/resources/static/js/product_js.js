e938543c19a11892e03ec63caab64e1a51197863
tls3254 <bikim9923@gmail.com>
2024-02-01 18:36:59 +0900

    통합 결재 완료

--- src/main/resources/static/js/product_js.js
+++ src/main/resources/static/js/product_js.js
@@ -5,16 +5,29 @@ const buyerName = document.getElementById("buyerName").value;
 const buyerTel = document.getElementById("buyerTel").value;
 const buyerAddr = document.getElementById("buyerAddr").value;
 const buyerPostcode = document.getElementById("buyerPostcode").value;
+const buyerProductId = document.getElementById("buyerProductId").value;
+const buyerUserId = document.getElementById("buyerUserId").value;
 
 const IMP = window.IMP;
 IMP.init("imp66770306");
 
+function generateMerchantUid() {
+    const now = new Date();
+    const year = now.getFullYear().toString().substr(2, 2); // 연도의 마지막 2자리만 가져옴
+    const month = (now.getMonth() + 1).toString().padStart(2, '0'); // 월을 두 자리로 만들어줌
+    const day = now.getDate().toString().padStart(2, '0'); // 일을 두 자리로 만들어줌
+    const timestamp = year + month + day; // yymmdd 형식으로 날짜 생성
+    const merchant_uid = "ORD" + timestamp + "-" + buyerProductId.toString().padStart(4, '0') + buyerUserId.toString().padStart(4, '0');
+    console.log(merchant_uid)
+    return merchant_uid;
+}
 function requestPay() {
+    var merchant_uid = generateMerchantUid();
     IMP.request_pay({
         pg: "kakaopay",
         tid: "8559342fd00616b5ff4b2faaea74ca65",
         pay_method: "card",
-        merchant_uid: "ORD20180131-0000011",
+        merchant_uid: merchant_uid,
         name: productName,
         amount: parseInt(productPrice),
         buyer_email: buyerEmail,
