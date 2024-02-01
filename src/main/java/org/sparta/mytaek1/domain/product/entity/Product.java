5a3c16854e3a4871236929d384cce96622a7af64
SeungbaeLee <aha224610@gmail.com>
2024-02-01 16:00:20 +0900

    feat : entity builder 추가

--- src/main/java/org/sparta/mytaek1/domain/product/entity/Product.java
+++ src/main/java/org/sparta/mytaek1/domain/product/entity/Product.java
@@ -2,6 +2,7 @@ package org.sparta.mytaek1.domain.product.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sparta.mytaek1.global.audit.Auditable;
@@ -16,4 +17,12 @@ public class Product extends Auditable {
     private String productName;
     private int productPrice;
     private int productStock;
+
+    @Builder
+    public Product(Long productId, String productName, int productPrice, int productStock) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productStock = productStock;
+    }
 }
