38039f1790ab1ac97b3bd2b04ce046140f53067a
KimDabomi <cdabomi@nate.com>
2024-02-02 19:17:26 +0900

    fix: 로그아웃 버그 수정, 방송 상세페이지로 연결

--- src/main/resources/static/js/broadcastList_js.js
+++ src/main/resources/static/js/broadcastList_js.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.view-details-button').forEach(function (button) {
-        button.addEventListener('click', function () {
-            var broadcastId = button.getAttribute('data-broadcast-id');
-            window.location.href = '/broadcasts/' + broadcastId;
-        });
+    button.addEventListener('click', function () {
+        const broadcastId = button.getAttribute('data-broadcast-id');
+        window.location.href = '/broadcasts/' + broadcastId;
     });
+});
--- src/main/resources/static/js/nav_js.js
+++ src/main/resources/static/js/nav_js.js
@@ -41,6 +41,4 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-function deleteCookie(name) {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
+function deleteCookie(name) { document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;'; }
