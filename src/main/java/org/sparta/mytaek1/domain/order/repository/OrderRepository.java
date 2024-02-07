275ebf276f0108de645242c7ee20f411981d82a1
SeungbaeLee <aha224610@gmail.com>
2024-02-08 01:36:27 +0900

    feat : 분산락으로 동시성 제어 동시성은 제어되지만 락을 획득하지 못할경우 false를 바로 반환해 주문생성이 실패 -> 사용자가 수동으로 계속 시도해야함

--- src/main/java/org/sparta/mytaek1/domain/order/repository/OrderRepository.java
+++ src/main/java/org/sparta/mytaek1/domain/order/repository/OrderRepository.java
@@ -5,10 +5,13 @@ import org.sparta.mytaek1.domain.product.entity.Product;
 import org.sparta.mytaek1.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
     Orders findByProductAndUser(Product product, User user);
 
     Optional<Orders> findByProduct(Product product);
+
+    List<Orders> findAllByProductProductId(Long productId);
 }
