e349d12b487b40fa24fcf47056435ed17236ce97
KimDabomi <cdabomi@nate.com>
2024-02-08 22:33:30 +0900

    feat: 결제 수단 카드/카카오페이로 변경

--- src/main/resources/static/js/order_js.js
+++ src/main/resources/static/js/order_js.js
@@ -40,6 +40,54 @@ function requestPay() {
             buyer_addr: buyerAddr,
             buyer_postcode: buyerPostcode
         }, function (rsp) {
+            console.log(rsp);
+            $.ajax({
+                type: 'POST',
+                url: '/verify/' + rsp.imp_uid
+            }).done(function(data) {
+                if(rsp.paid_amount === data.response.amount){
+                    $.ajax({
+                        type: 'PUT',
+                        url: `/api/orders/${orderId}/paymentStatus`
+                    })
+                    alert("결제 성공");
+                    window.location.href="/my-page";
+                } else {
+                    alert("결제 실패");
+                }
+            });
+        });
+    } else {
+        alert('로그인 후에 결제할 수 있습니다.');
+        window.location.href = '/api/user/login-page';
+    }
+}
+
+function requestKakaoPay() {
+    const isAuthenticated = checkCookieExistence('Authorization');
+    const orderContainer = document.querySelector('.order_container');
+    const orderId = orderContainer.getAttribute('data-order-id');
+
+
+    if (isAuthenticated) {
+        const quantity = parseInt(document.getElementById("orderQuantity").textContent);
+        const totalPrice = parseInt(productPrice) * quantity;
+        const merchant_uid = generateMerchantUid();
+
+        IMP.request_pay({
+            pg: "kakaopay",
+            tid: "8559342fd00616b5ff4b2faaea74ca65",
+            pay_method: "card",
+            merchant_uid: merchant_uid,
+            name: productName,
+            amount: totalPrice,
+            buyer_email: buyerEmail,
+            buyer_name: buyerName,
+            buyer_tel: buyerTel,
+            buyer_addr: buyerAddr,
+            buyer_postcode: buyerPostcode
+        }, function (rsp){
+            console.log(rsp);
             $.ajax({
                 type: 'POST',
                 url: '/verify/' + rsp.imp_uid
