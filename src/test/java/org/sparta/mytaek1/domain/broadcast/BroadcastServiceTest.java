dd47c61ffe03b5dc697a29d5ac0d622f9102afc7
SeungbaeLee <aha224610@gmail.com>
2024-02-13 06:09:15 +0900

    refactor : 구조 변경에 따른 테스트코드 수정

--- src/test/java/org/sparta/mytaek1/domain/broadcast/BroadcastServiceTest.java
+++ src/test/java/org/sparta/mytaek1/domain/broadcast/BroadcastServiceTest.java
@@ -1,21 +1,34 @@
 package org.sparta.mytaek1.domain.broadcast;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sparta.mytaek1.domain.broadcast.dto.BroadcastRequestDto;
+import org.sparta.mytaek1.domain.broadcast.entity.Broadcast;
 import org.sparta.mytaek1.domain.broadcast.repository.BroadcastRepository;
 import org.sparta.mytaek1.domain.broadcast.service.BroadcastService;
+import org.sparta.mytaek1.domain.product.entity.Product;
 import org.sparta.mytaek1.domain.product.repository.ProductRepository;
 import org.sparta.mytaek1.domain.product.service.ProductService;
+import org.sparta.mytaek1.domain.stock.entity.Stock;
+import org.sparta.mytaek1.domain.stock.service.StockService;
+import org.sparta.mytaek1.domain.user.entity.User;
 import org.sparta.mytaek1.domain.user.repository.UserRepository;
+import org.sparta.mytaek1.global.security.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
 @Rollback
@@ -28,37 +41,41 @@ public class BroadcastServiceTest {
 
     private final BroadcastRepository broadcastRepository;
 
+    private final StockService stockService;
+
     private final ProductRepository productRepository;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    public BroadcastServiceTest(BroadcastService broadcastService, UserRepository userRepository, ProductService productService, BroadcastRepository broadcastRepository, ProductRepository productRepository) {
+    public BroadcastServiceTest(BroadcastService broadcastService, UserRepository userRepository, ProductService productService, BroadcastRepository broadcastRepository, ProductRepository productRepository, StockService stockService) {
         this.broadcastService = broadcastService;
         this.userRepository = userRepository;
         this.broadcastRepository = broadcastRepository;
         this.productRepository = productRepository;
+        this.stockService = stockService;
     }
 
-//    @Test
-//    @Transactional
-//    @DisplayName("Product, Broadcast 생성")
-//    public void createBroadcastTest() {
-//        User user = new User("Test User2", "test2@example.com", "password1234@", "streamKey", "123-456-7890", "Test Address", "12345");
-//        userRepository.save(user);
-//        UserDetailsImpl userDetails = new UserDetailsImpl(user);
-//        BroadcastRequestDto requestDto = new BroadcastRequestDto("Test Title", "Test Description", "Test Product", "Product Description", 100, 10);
-//        broadcastService.createBroadcast(userDetails, requestDto);
-//        Broadcast broadcast = broadcastRepository.findByBroadcastTitle("Test Title");
-//
-//        assertEquals("Test Title", broadcast.getBroadcastTitle());
-//        assertEquals("Test Description", broadcast.getBroadcastDescription());
-//        assertEquals("Test User2", broadcast.getUser().getUserName());
-//        assertEquals("Test Product", broadcast.getProduct().getProductName());
-//        assertEquals(100, broadcast.getProduct().getProductPrice());
-//        assertEquals(10, broadcast.getProduct().getProductStock());
-//    }
+    @Test
+    @Transactional
+    @DisplayName("Product, Broadcast 생성")
+    public void createBroadcastTest() {
+        User user = new User("Test User2", "test2@example.com", "password1234@", "streamKey", "123-456-7890", "Test Address", "12345");
+        userRepository.save(user);
+        UserDetailsImpl userDetails = new UserDetailsImpl(user);
+        BroadcastRequestDto requestDto = new BroadcastRequestDto("Test Title", "Test Description", "Test Product", "Product Description", 100, 10);
+        broadcastService.createBroadcast(userDetails, requestDto);
+        Broadcast broadcast = broadcastRepository.findByBroadcastTitle("Test Title");
+        Stock stock = stockService.findStockByProduct(broadcast.getProduct().getProductId());
+
+        assertEquals("Test Title", broadcast.getBroadcastTitle());
+        assertEquals("Test Description", broadcast.getBroadcastDescription());
+        assertEquals("Test User2", broadcast.getUser().getUserName());
+        assertEquals("Test Product", broadcast.getProduct().getProductName());
+        assertEquals(100, broadcast.getProduct().getProductPrice());
+        assertEquals(10, stock.getProductStock());
+    }
 
     @Test
     @DisplayName("Null 체크")
@@ -89,7 +106,7 @@ public class BroadcastServiceTest {
 //    public void showBroadcastTest() throws Exception {
 //
 //        User user = new User("TestUser", "test@user.com", "Password123!", "TestStreamKey", "010-1234-1234", "ASDF", "12345");
-//        Product product = new Product("TestProduct", "TestProductDescription", 10000, 10);
+//        Product product = new Product("TestProduct", "TestProductDescription", 10000);
 //        Broadcast broadcast = new Broadcast("TestBroadcast", "TestDescription", user, product);
 //        userRepository.save(user);
 //        productRepository.save(product);
