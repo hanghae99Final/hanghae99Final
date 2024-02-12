e3ecd9a36eb54e88cad7f8b5425978d4d83f6a68
SeungbaeLee <aha224610@gmail.com>
2024-02-13 06:52:26 +0900

    fix : lockName 수정(userName -> productName)

--- src/main/java/org/sparta/mytaek1/domain/order/controller/OrderController.java
+++ src/main/java/org/sparta/mytaek1/domain/order/controller/OrderController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.sparta.mytaek1.domain.order.dto.OrderRequestDto;
 import org.sparta.mytaek1.domain.order.dto.OrderResponseDto;
 import org.sparta.mytaek1.domain.order.service.OrderService;
+import org.sparta.mytaek1.domain.product.entity.Product;
+import org.sparta.mytaek1.domain.product.service.ProductService;
 import org.sparta.mytaek1.domain.user.entity.User;
 import org.sparta.mytaek1.global.security.UserDetailsImpl;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +22,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OrderController {
 
     private final OrderService orderService;
+    private final ProductService productService;
 
     @PostMapping("/products/{productId}/orders")
     public ResponseEntity<OrderResponseDto> createOrder(@PathVariable Long productId,
                                               @RequestBody OrderRequestDto orderRequestDto,
                                               @AuthenticationPrincipal UserDetailsImpl userDetails){
         User user = userDetails.getUser();
-        OrderResponseDto orderResponseDto = orderService.createOrder(user.getUserName(), productId, orderRequestDto, user);
+        Product product = productService.findProduct(productId);
+        OrderResponseDto orderResponseDto = orderService.createOrder(product.getProductName(), productId, orderRequestDto, user);
         return ResponseEntity.ok(orderResponseDto);
     }
 }
