529a4d183a93f79d8ceea3f516c97b49ab67fd25
KimDabomi <cdabomi@nate.com>
2024-02-14 18:41:12 +0900

    feat: 빌링키로 결제시 결제 상태 반영, 마이페이지에 상태 표시 추가

--- src/main/java/org/sparta/mytaek1/domain/broadcast/dto/BroadcastResponseDto.java
+++ src/main/java/org/sparta/mytaek1/domain/broadcast/dto/BroadcastResponseDto.java
@@ -10,23 +10,23 @@ import java.util.stream.Collectors;
 public class BroadcastResponseDto {
 
     private final long broadcastId;
-    private final String broadCastTitle;
-    private final String broadCastDescription;
+    private final String broadcastTitle;
+    private final String broadcastDescription;
     private final String userName;
     private final String productName;
 
-    public BroadcastResponseDto(long broadcastId, String broadCastTitle, String broadCastDescription, String userName, String productName) {
+    public BroadcastResponseDto(long broadcastId, String broadcastTitle, String broadcastDescription, String userName, String productName) {
         this.broadcastId = broadcastId;
-        this.broadCastTitle = broadCastTitle;
-        this.broadCastDescription = broadCastDescription;
+        this.broadcastTitle = broadcastTitle;
+        this.broadcastDescription = broadcastDescription;
         this.userName = userName;
         this.productName = productName;
     }
 
     public BroadcastResponseDto(Broadcast broadcast) {
         this.broadcastId = broadcast.getBroadcastId();
-        this.broadCastTitle = broadcast.getBroadcastTitle();
-        this.broadCastDescription = broadcast.getBroadcastDescription();
+        this.broadcastTitle = broadcast.getBroadcastTitle();
+        this.broadcastDescription = broadcast.getBroadcastDescription();
         this.userName = broadcast.getUser().getUserName();
         this.productName = broadcast.getProduct().getProductName();
     }
