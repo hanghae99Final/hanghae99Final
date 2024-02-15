778994ceac3eee0fc3baf625cefd54d4399b13eb
SeungbaeLee <aha224610@gmail.com>
2024-02-15 12:02:06 +0900

    feat : healthcheck

---
+++ src/main/java/org/sparta/mytaek1/global/health/HealthController.java
@@ -0,0 +1,13 @@
+package org.sparta.mytaek1.global.health;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HealthController {
+
+    @GetMapping("/healthcheck")
+    public String healthcheck() {
+        return "OK";
+    }
+}
