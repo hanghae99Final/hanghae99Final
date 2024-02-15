50ef82debeb0faa30168ce881c8555d39953f813
SeungbaeLee <aha224610@gmail.com>
2024-02-15 15:20:04 +0900

    feat : healthcheck

--- src/main/java/org/sparta/mytaek1/global/health/HealthController.java
+++ src/main/java/org/sparta/mytaek1/global/health/HealthController.java
@@ -1,9 +1,9 @@
 package org.sparta.mytaek1.global.health;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HealthController {
 
     @GetMapping("/healthcheck")
