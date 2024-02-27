22e1c81aad616dc66fb742ee97747ecef0314021
KimDabomi <cdabomi@nate.com>
2024-02-27 21:34:48 +0900

    docs: sh파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,23 +1,3 @@
-# JAR="/home/ubuntu/build/libs/mytaek1-0.0.1-SNAPSHOT.jar"
-
-# chmod +x /var/log/aws/codedeploy-agent/
-
-# APP_LOG="/var/log/aws/codedeploy-agent/application.log"
-# ERROR_LOG="/var/log/aws/codedeploy-agent/error.log"
-# START_LOG="/var/log/aws/codedeploy-agent/start.log"
-
-# NOW=$(date +%c)
-
-# # echo "[$NOW] $JAR 복사" >> $START_LOG
-# # cp /home/ubuntu/build/libs/mytaek1-0.0.1-SNAPSHOT.jar $JAR
-
-# echo "[$NOW] > $JAR 실행" >> $START_LOG
-# nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
-
-# SERVICE_PID=$(pgrep -f $JAR)
-# echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
-
-
 #!/bin/bash
 
 
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,21 +1,9 @@
-# JAR="../build/libs/mytaek1-0.0.1-SNAPSHOT.jar"
-# STOP_LOG="/var/log/aws/codedeploy-agent/stop.log"
-# SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
-
-# if [ -z "$SERVICE_PID" ]; then
-#   echo "서비스 NouFound" >> $STOP_LOG
-# else
-#   echo "서비스 종료 " >> $STOP_LOG
-#   kill "$SERVICE_PID"
-# fi
-
-
 #!/bin/bash
 
 ROOT_PATH="/home/ubuntu/spring-github-action"
 JAR="$ROOT_PATH/application.jar"
 STOP_LOG="/var/log/aws/codedeploy-agent/stop.log"
-SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+SERVICE_PID=$(pgrep -f $JAR)
 
 if [ -z "$SERVICE_PID" ]; then
   echo "서비스 NotFound" >> $STOP_LOG
