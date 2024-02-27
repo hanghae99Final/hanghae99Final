2974b3e7c3674e8612c20d71f0d6f91ba8cfdda6
Kim Dabomi <110092175+KimDabomi@users.noreply.github.com>
2024-02-27 18:41:02 +0900

    docs: Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,6 @@
-JAR="$ROOT_PATH/application.jar"
-STOP_LOG="$ROOT_PATH/stop.log"
+# JAR="$ROOT_PATH/application.jar"
+JAR="../build/libs/mytaek1-0.0.1-SNAPSHOT.jar"
+STOP_LOG="/log/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
@@ -7,5 +8,4 @@ if [ -z "$SERVICE_PID" ]; then
 else
   echo "서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
-  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 ��� 명령어 사용
 fi
