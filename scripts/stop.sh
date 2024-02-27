4191486dc8ef056c76283f149bfc184b2bf3da18
SeungbaeLee <aha224610@gmail.com>
2024-02-27 17:35:20 +0900

    feat : stop.sh 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,6 +2,7 @@ JAR="$ROOT_PATH/application.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
+
 if [ -z "$SERVICE_PID" ]; then
   echo "서비스 NouFound" >> $STOP_LOG
 else
