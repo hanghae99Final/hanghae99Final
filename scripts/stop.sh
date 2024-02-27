fbcf7e206ed4d0cc0b1471ecaca561b883676e0a
Kim Dabomi <110092175+KimDabomi@users.noreply.github.com>
2024-02-27 18:45:44 +0900

    dosc: Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 # JAR="$ROOT_PATH/application.jar"
 JAR="../build/libs/mytaek1-0.0.1-SNAPSHOT.jar"
-STOP_LOG="/log/stop.log"
+STOP_LOG="/var/log/aws/codedeploy-agent/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
