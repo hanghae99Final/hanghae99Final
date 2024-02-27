c5cd262aa4925cf07e87f59f7eedd7b983d49090
Kim Dabomi <110092175+KimDabomi@users.noreply.github.com>
2024-02-27 19:23:28 +0900

    docs: Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,36 @@
-JAR="/home/ubuntu/build/libs/mytaek1-0.0.1-SNAPSHOT.jar"
+# JAR="/home/ubuntu/build/libs/mytaek1-0.0.1-SNAPSHOT.jar"
 
-chmod +x /var/log/aws/codedeploy-agent/
+# chmod +x /var/log/aws/codedeploy-agent/
 
-APP_LOG="/var/log/aws/codedeploy-agent/application.log"
-ERROR_LOG="/var/log/aws/codedeploy-agent/error.log"
-START_LOG="/var/log/aws/codedeploy-agent/start.log"
+# APP_LOG="/var/log/aws/codedeploy-agent/application.log"
+# ERROR_LOG="/var/log/aws/codedeploy-agent/error.log"
+# START_LOG="/var/log/aws/codedeploy-agent/start.log"
+
+# NOW=$(date +%c)
+
+# # echo "[$NOW] $JAR 복사" >> $START_LOG
+# # cp /home/ubuntu/build/libs/mytaek1-0.0.1-SNAPSHOT.jar $JAR
+
+# echo "[$NOW] > $JAR 실행" >> $START_LOG
+# nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+
+# SERVICE_PID=$(pgrep -f $JAR)
+# echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+
+
+#!/bin/bash
+
+ROOT_PATH="/home/ubuntu/spring-github-action"
+JAR="$ROOT_PATH/application.jar"
+
+APP_LOG="$ROOT_PATH/application.log"
+ERROR_LOG="$ROOT_PATH/error.log"
+START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-# echo "[$NOW] $JAR 복사" >> $START_LOG
-# cp /home/ubuntu/build/libs/mytaek1-0.0.1-SNAPSHOT.jar $JAR
+echo "[$NOW] $JAR 복사" >> $START_LOG
+cp $ROOT_PATH/build/libs/spring-github-action-1.0.0.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
