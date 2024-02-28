8922cc585dade94ef28f4ef7f3630486382c8c7e
tls3254 <bikim9923@gmail.com>
2024-02-29 00:22:21 +0900

    feat : 결제 환불 반 구현

---
+++ src/main/java/org/sparta/mytaek1/domain/payment/dto/CancelPayment.java
@@ -0,0 +1,10 @@
+package org.sparta.mytaek1.domain.payment.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CancelPayment {
+    private String merchantUid;
+}
